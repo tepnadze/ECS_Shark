a2de0e3d073c7f23035f49d594b263029c156dea
tepnadze <56294777+tepnadze@users.noreply.github.com>
2021-12-17 14:48:42 +0200

    Update_1(17.12.21)
    1)Fix ECS Rotation
    2)Add  Wave Shader

--- Assets/Scripts/Movement/Systems/SharkMovementSystem.cs
+++ Assets/Scripts/Movement/Systems/SharkMovementSystem.cs
@@ -1,15 +1,37 @@
+using System.Numerics;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
+using Quaternion = UnityEngine.Quaternion;
 
-
+[UpdateAfter(typeof(JoystickInputSystem))]
 public class SharkMovementSystem : SystemBase
 {
     protected override void OnUpdate()
     {
-        Entities.ForEach((ref Translation translation , ref JoystickInputData joystick_input) =>
+        sharkTranslation();
+        sharkRotation();
+    }
+
+    private void sharkRotation()
+    {
+        Entities.ForEach((ref Rotation rotation, ref JoystickInputData joystick_input) =>
         {
-            translation.Value += new float3(joystick_input.xPos, 0f, joystick_input.zPos);
-        }).WithoutBurst().Run(); 
+            if (joystick_input.xPos == 0 && joystick_input.zPos == 0) return;
+            
+            var pos                    = new float3(joystick_input.xPos, 0f, joystick_input.zPos);
+            var look_rotation = quaternion.LookRotation(pos, math.up());
+
+            rotation.Value             = math.slerp(rotation.Value, look_rotation, Time.DeltaTime * 4f);
+            
+        }).WithoutBurst().Run();
+    }
+
+    private void sharkTranslation()
+    {
+        Entities.ForEach((ref Translation translation, ref JoystickInputData joystick_input) =>
+        {
+            translation.Value += new float3(joystick_input.xPos, 0f, joystick_input.zPos) * Time.DeltaTime * 4f;
+        }).WithoutBurst().Run();
     }
 }
